f3071435fbd44f9d9ccc7ceb4658cc3189962254
Xiangyun Chi <xiangyun.chi@sky.uk>
2017-05-02 14:19:22 +0100

    add sayIntro

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 require('isomorphic-fetch');
 const { Wechaty } = require('wechaty');
+const spawn = require('child_process').spawn;
 
 const chatterbotServer = 'http://localhost:5000';
-Wechaty.instance()
+const sayIntro = () => {
+    contact.say('生日快乐！');
+
+    contact.say('我是猪小二嫰儿，由猪小大嫩儿开发给池小蔫当礼物！赶紧试试调戏我吧。');
+};
+
+Wechaty.instance({ profile:'ernenr' })
   .on('scan', (url, code) =>
     console.log(`Scan QR Code to login: ${code}\n${url}`)
   )
@@ -20,5 +27,13 @@ Wechaty.instance()
       const retText = await blob.text();
       await message.say(retText);
     }
+  })
+  .on('friend', (contact, request) => {
+    if (request) return;
+    sayIntro();
+  })
+  .on('error', () => {
+    process.exit(1);
+    spawn('node index.js');
   })
   .init();
